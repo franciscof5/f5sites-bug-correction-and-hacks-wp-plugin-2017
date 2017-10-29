74202df540deaed7e8a226c6ef65dfd64d1d85e1
Francisco Mat <francisco@f5sites.com>
2017-10-29 13:55:30 -0200

    first v, timeline plugin

---
+++ f5sites-bug-correction-hack-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: F5 Sites | Bug Correction Hack Plugin
+Plugin URI: https://www.f5sites.com/software/wordpress/f5sites-bug-correction-hack-plugin/
+Plugin Description: When some plugin has a bug and we dont modify it source code to avoid update issues, so we make changes in our custom hack plugin. WordPress F5 Sites DEV projects. 
+Author: Francisco Matelli Matulovic
+Author URI: https://www.franciscomat.com/
+License: GPLv3
+Tags: mu-plugins */
+
+?>
+<style type="text/css">
+	ul.post-meta {display: none;}
+</style>
