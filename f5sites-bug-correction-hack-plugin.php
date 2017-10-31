8d0160aa7e3dd460a140f39f6acec82540d59ca3
Francisco Mat <francisco@f5sites.com>
2017-10-31 19:08:09 -0200

    insert wp hook

--- f5sites-bug-correction-hack-plugin.php
+++ f5sites-bug-correction-hack-plugin.php
@@ -2,13 +2,18 @@
 /*
 Plugin Name: F5 Sites | Bug Correction Hack Plugin
 Plugin URI: https://www.f5sites.com/software/wordpress/f5sites-bug-correction-hack-plugin/
-Plugin Description: When some plugin has a bug and we dont modify it source code to avoid update issues, so we make changes in our custom hack plugin. WordPress F5 Sites DEV projects. 
+Plugin Description: When some 3rd part plugin has a bug and we dont modify it source code to avoid update issues, so we make changes in our custom hack plugin. WordPress F5 Sites DEV projects. 
 Author: Francisco Matelli Matulovic
 Author URI: https://www.franciscomat.com/
 License: GPLv3
 Tags: mu-plugins */
 
-?>
-<style type="text/css">
-	ul.post-meta {display: none;}
-</style>
+
+if(!is_admin()) { 
+	add_action('init', 'correct_bugs_f5sites');
+}
+function correct_bugs_f5sites() { ?>
+	<style type="text/css">
+		ul.post-meta {display: none;}
+	</style>
+<?php } ?>
